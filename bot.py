dffcaf6561c6c30c79004f9ef4e2f5d4d32d6e47
Gaurav Gupta <31289268+1999gauravgupta@users.noreply.github.com>
2018-06-17 00:38:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def on_message(message):
     Words=["dick","pussy","motherfucker","asshole","son of a bitch","dickhead","bitch","dick head","cunt","faggot","fag","nigga","niger","nigger","slut"]
 
     if  message.author.bot==False:
-       if if message.server.id not in [281793428793196544 , 265828729970753537]:
+      if message.server.id not in [281793428793196544 , 265828729970753537]:
         if message.author.id in owner:
             pass
         else:
