0f827a5a67b84fb4062ba7c052b7a618514d1dc0
Gaurav Gupta <31289268+1999gauravgupta@users.noreply.github.com>
2018-06-19 14:01:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    Words=["dick","pussy","motherfucker","asshole","son of a bitch","dickhead","bitch","dick head","cunt","faggot","fag","nigga","niger","nigger","slut"]
+    Words=["dick","pussy","motherfucker","whore", "asshole","son of a bitch","dickhead","bitch","dick head","cunt","faggot","fag","nigga","niger","nigger","slut"]
     with open("serverf.txt", "r") as f1:
         list1=f1.readlines()
         for i in list1:
