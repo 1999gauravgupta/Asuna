67c6810af75faaa321d3bc029a8e210cf7302c19
Gaurav Gupta <31289268+1999gauravgupta@users.noreply.github.com>
2018-06-20 13:13:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1054,7 +1054,7 @@ async def lyrics(ctx,*,song):
             await bot.say("Lyrics not available for this song. Are you sure you entered correct details?")
             print(e)
 @bot.command(pass_context=True)
-async def big( ctx, emoji : FailsafeEmojiConverter):
+async def big( ctx, emoji : discord.Emoji):
     try:
         e = discord.Embed(title="{}".format(emoji.name), color=0xf7d28c)
         p=(emoji.url).replace("discordapp.com/api","cdn.discordapp.com")
