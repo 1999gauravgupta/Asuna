c89cef7454617f89f46b297158da0a901c352a2e
Gaurav Gupta <31289268+1999gauravgupta@users.noreply.github.com>
2018-06-29 12:39:19 +0530

    Add files via upload

--- games.py
+++ games.py
@@ -9,22 +9,38 @@ import string
 class GAMES():
     def __init__(self, bot):
         self.bot = bot
+ 
+    def card_drawer(deck):
+        restrict=["QH","QS"]
+        draw = requests.get('https://deckofcardsapi.com/api/deck/{}/draw/?count=1'.format(deck))
+        draw = json.loads(draw.content.decode('utf-8'))
+        if draw["cards"][0]["code"] in restrict:
+            card_drawer(deck)
+        else:
+            return draw
+    
+    def card_to_emoji(card):       
+        for a in bot.guilds:
+            if a.id==461497700160897035:
+                for emoji in a.emojis:
+                    if emoji.name==card:
+                        return "<:{}:{}>".format(emoji.name,emoji.id)
 
     @commands.command(name='bj', aliases=['blackjack'])
     async def bj(self, ctx):
         d_valuea = []
+        u_cards = []
+        d_cards = []
+        u_value = 0
+        d_value=0
         fc = ['KING', 'QUEEN', 'JACK']
         def check(m):
             return m.author.id == ctx.author.id and m.channel == ctx.channel
         r = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
         r = json.loads(r.content.decode('utf-8'))
         deck = r['deck_id']
-        draw = requests.get('https://deckofcardsapi.com/api/deck/{}/draw/?count=2'.format(deck))
-        draw = json.loads(draw.content.decode('utf-8'))
-        u_value = 0
-        u_cards = []
-        d_cards = []
         for j in range(2):
+            draw=card_drawer(deck)
             if draw['cards'][j]['value'] in fc:
                 u_value += 10
             elif draw['cards'][j]['value'] == 'ACE':
@@ -32,9 +48,7 @@ class GAMES():
             else:
                 u_value += int(draw['cards'][j]['value'])
             u_cards.append(draw['cards'][j]['code'])
-        d_value = 0
-        draw = requests.get('https://deckofcardsapi.com/api/deck/{}/draw/?count=1'.format(deck))
-        draw = json.loads(draw.content.decode('utf-8'))
+        draw=card_drawer(deck)
         if draw['cards'][0]['value'] in fc:
             d_value += 10
         elif draw['cards'][0]['value'] == 'ACE':
@@ -43,12 +57,14 @@ class GAMES():
             d_value += int(draw['cards'][0]['value'])
         d_cards.append(draw['cards'][0]['code'])
         d_valuea.append(d_value)
-        q = ''
-        w = ''
-        for a in u_cards:
-            q += a + ' '
-        for b in d_cards:
-            w += b + ' '
+        q=""
+        w=""
+        for i in u_cards:
+            a=card_to_emoji(i)
+            q+=a+" "
+        for i in d_cards:
+            a=card_to_emoji(i)
+            w+=a+" "
         embed = discord.Embed(title='Blackjack', color=16241292)
         embed.add_field(name='User Cards', value=q)
         embed.add_field(name='Dealer Cards', value=w)
@@ -56,13 +72,11 @@ class GAMES():
         embed.add_field(name='Dealer Cards value', value=d_value, inline=True)
         embed.add_field(name='\u200b', value='Hit/Stand/Double Down')
         await ctx.send(embed=embed)  # print(u_value,d_value)
-        def check(m):
-            return m.author.id == ctx.author.id and m.channel == ctx.channel
+
         option = await self.bot.wait_for('message', check = check)
         option = str(option.content)
         if option.lower() == 'hit':
-            draw = requests.get('https://deckofcardsapi.com/api/deck/{}/draw/?count=1'.format(deck))
-            draw = json.loads(draw.content.decode('utf-8'))
+            draw=card_drawer(deck)
             if draw['cards'][0]['value'] in fc:
                 u_value += 10
             elif draw['cards'][0]['value'] == 'ACE':
@@ -71,8 +85,7 @@ class GAMES():
                 u_value += int(draw['cards'][0]['value'])
             u_cards.append(draw['cards'][0]['code'])
         elif option.lower() == 'double down':
-            draw = requests.get('https://deckofcardsapi.com/api/deck/{}/draw/?count=1'.format(deck))
-            draw = json.loads(draw.content.decode('utf-8'))
+            draw=card_drawer(deck)
             if draw['cards'][0]['value'] in fc:
                 u_value += 10
             elif draw['cards'][0]['value'] == 'ACE':
@@ -82,8 +95,7 @@ class GAMES():
             u_cards.append(draw['cards'][0]['code'])
         else:
             pass
-        draw = requests.get('https://deckofcardsapi.com/api/deck/{}/draw/?count=2'.format(deck))
-        draw = json.loads(draw.content.decode('utf-8'))
+        draw=card_drawer(deck)
         for k in range(2):
             if draw['cards'][k]['value'] in fc:
                 d_value += 10
@@ -96,17 +108,16 @@ class GAMES():
         for i in d_valuea:
             if i <= 21:
                 d_value = i
-        try:
-            await ctx.message.delete()
-        except Exception:
-            pass
+        q=""
+        w=""
+        for i in u_cards:
+            a=card_to_emoji(i)
+            q+=a+" "
+        for i in d_cards:
+            a=card_to_emoji(i)
+            w+=a+" "
+
         if u_value > 21: 
-            q = ''
-            w = ''
-            for a in u_cards:
-                q += a + ' '
-            for b in d_cards:
-                w += b + ' '
             embed = discord.Embed(title='Blackjack', color=15875132)
             embed.add_field(name='User Cards', value=q)
             embed.add_field(name='Dealer Cards', value=w)
@@ -114,12 +125,6 @@ class GAMES():
             embed.add_field(name='Dealer Cards value', value=d_value, inline=True)
             await ctx.send(embed=embed)
         elif (u_value <= 21) and (u_value >= d_value):
-            q = ''
-            w = ''
-            for a in u_cards:
-                q += a + ' '
-            for b in d_cards:
-                w += b + ' '
             embed = discord.Embed(title='Blackjack', color=3994233)
             embed.add_field(name='User Cards', value=q)
             embed.add_field(name='Dealer Cards', value=w)
@@ -127,12 +132,6 @@ class GAMES():
             embed.add_field(name='Dealer Cards value', value=d_value, inline=True)
             await ctx.send(embed=embed)
         else:
-            q = ''
-            w = ''
-            for a in u_cards:
-                q += a + ' '
-            for b in d_cards:
-                w += b + ' '
             embed = discord.Embed(title='Blackjack', color=15875132)
             embed.add_field(name='User Cards', value=q)
             embed.add_field(name='Dealer Cards', value=w)
